ac3f2ec4285ccd59896e99471e81865d2cc04106
FedericoCapitanio <107490782+FedericoCapitanio@users.noreply.github.com>
2026-02-28 11:17:57 +0100

    default not be initially blocked or locked

--- tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
@@ -5,8 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private SmartDoorLock lock;
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void lockShouldNotBeLocked() {
+
+        lock = new SmartDoorLockImpl();
+        assertFalse(lock.isLocked());
     }
+
+    @Test
+    public void lockShouldNotBeBlocked() {
+
+        lock = new SmartDoorLockImpl();
+        assertFalse(lock.isBlocked());
+
+    }
+
 }

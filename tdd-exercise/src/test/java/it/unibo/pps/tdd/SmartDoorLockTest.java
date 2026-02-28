712359f65df4298786fad2829cb9b6dd2760a77b
FedericoCapitanio <107490782+FedericoCapitanio@users.noreply.github.com>
2026-02-28 11:35:25 +0100

    refactor

--- tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/it/unibo/pps/tdd/SmartDoorLockTest.java
@@ -1,5 +1,6 @@
 package it.unibo.pps.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,18 +9,21 @@ public class SmartDoorLockTest {
 
     private SmartDoorLock lock;
 
+    @BeforeEach
+    public void init() {
+        this.lock = new SmartDoorLockImpl();
+    }
+
     @Test
     public void lockShouldNotBeLocked() {
 
-        lock = new SmartDoorLockImpl();
-        assertFalse(lock.isLocked());
+        assertFalse(this.lock.isLocked());
     }
 
     @Test
     public void lockShouldNotBeBlocked() {
 
-        lock = new SmartDoorLockImpl();
-        assertFalse(lock.isBlocked());
+        assertFalse(this.lock.isBlocked());
 
     }
 

7e052cfa3eddd4fbcfa2cb0abf76fe4993e3b6ac
FedericoCapitanio <107490782+FedericoCapitanio@users.noreply.github.com>
2026-03-01 13:02:11 +0100

    circular queue interface defined

--- tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularQueue.java
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularQueue.java
@@ -13,4 +13,35 @@ package it.unibo.pps.tdd;
  *   - Introduce methods in the interface in order to make the tests pass.
  *   - Refactor
  */
-public interface CircularQueue { }
+public interface CircularQueue {
+
+    /**
+     * Pushes an integer onto the queue.
+     *
+     * @param value The integer to push into the queue.
+     */
+    void push(int value);
+
+    /**
+     * Pushes an integer onto the queue.
+     *
+     * @throws IllegalStateException if the queue is empty.
+     */
+    void remove();
+
+    /**
+     * Get the max queue size.
+     *
+     * @return integer value.
+     */
+    int maxCapacity();
+
+    /**
+     * Retrieves, but does not remove, the top element of the stack.
+     *
+     * @return The top element of the stack.
+     * @throws IllegalStateException if the stack is empty.
+     */
+    int peek();
+
+}

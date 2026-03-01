0d3efd771a689105b7e3b8978dcf8ed126ee91b7
FedericoCapitanio <107490782+FedericoCapitanio@users.noreply.github.com>
2026-03-01 13:02:59 +0100

    circular queue class created

---
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularQueueImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.pps.tdd;
+
+public class CircularQueueImpl implements CircularQueue {
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int maxCapacity() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+}
